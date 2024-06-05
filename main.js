e0d3d137395c7f5921f14f4e07178bfe4cce4f66
Chris Nelson <chrisnelson7791@gmail.com>
2024-06-05 12:00:26 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Write the "square"-function here
+
+function square(x) {
+  return x ** 2
+}
+
+
+// params: x    a number
+// returns: x   returns x squared
+// e.g. square(5)
+// results in 25
+
+
+// return x ** 2
